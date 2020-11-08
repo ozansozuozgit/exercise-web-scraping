c3b463992702c334722fb73e895399ce00e1fa60
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-08 11:41:30 +0300

    set all firebase env

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ const firebase = require('firebase');
 
 const firebaseConfig = {
   apiKey: process.env.apiKey,
-  authDomain: 'workout-creator-29a33.firebaseapp.com',
-  databaseURL: 'https://workout-creator-29a33.firebaseio.com',
-  projectId: 'workout-creator-29a33',
-  storageBucket: 'workout-creator-29a33.appspot.com',
-  messagingSenderId: '509611031953',
-  appId: '1:509611031953:web:92403587aa442f4ee46e30',
-  measurementId: 'G-MBTVXEK89D',
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
